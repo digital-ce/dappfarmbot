a200d000407204f63aaf99f15629844224afc127
digital-ce <itzdigitalce@gmail.com>
2024-09-08 01:59:53 +0100

    Update about.ts

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -1,12 +1,10 @@
 import { Context } from 'telegraf';
 import createDebug from 'debug';
 
-import { author, name, version } from '../../package.json';
-
 const debug = createDebug('bot:about_command');
 
 const about = () => async (ctx: Context) => {
-  const message = `*${name} ${version}*\n${author}`;
+  const message = `🌟 *Dapp Farm*\n\nWelcome to Dapp Farm, where you can play games, earn tokens, and learn blockchain skills! Start your journey today and unlock the full potential of decentralized apps. 🚀`;
   debug(`Triggered "about" command with message \n${message}`);
   await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' });
 };
